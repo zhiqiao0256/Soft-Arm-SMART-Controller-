10841b4aa5a6e31f830be3e46b0b8da6cb4cde5f
zhiqiao0256 <zhiqiao0256@gmail.com>
2020-07-03 16:38:28 -0700

    Update low_level_main_0_2.m

--- low_level_main_0_2.m
+++ low_level_main_0_2.m
@@ -12,19 +12,27 @@ else
     par_set.flag_read_mat=1;
 end
 %
-par_set.trail25psi=[];
-par_set.trail20psi=[];
-par_set.trail15psi=[];
-par_set.trail10psi=[];
-par_set.trail5psi=[];
+par_set.trail_0_25psi=[];
+par_set.trail_0_20psi=[];
+par_set.trail_0_15psi=[];
+par_set.trail_0_10psi=[];
+par_set.trail_0_5psi=[];
+par_set.trail_5_25psi=[];
+par_set.trail_10_25psi=[];
+par_set.trail_15_25psi=[];
+par_set.trail_20_25psi=[];
 %
 %% Read txt exp. data
 if par_set.flag_read_exp==1
-    par_set.trail25psi=low_level_exp_0_1(par_set.trail25psi,1);
-    par_set.trail20psi=low_level_exp_0_1(par_set.trail20psi,2);
-    par_set.trail15psi=low_level_exp_0_1(par_set.trail15psi,3);
-    par_set.trail10psi=low_level_exp_0_1(par_set.trail10psi,4);
-    par_set.trail5psi=low_level_exp_0_1(par_set.trail5psi,5);
+    par_set.trail_0_25psi=low_level_exp_0_1(par_set.trail_0_25psi,1);
+    par_set.trail_0_20psi=low_level_exp_0_1(par_set.trail_0_20psi,2);
+    par_set.trail_0_15psi=low_level_exp_0_1(par_set.trail_0_15psi,3);
+    par_set.trail_0_10psi=low_level_exp_0_1(par_set.trail_0_10psi,4);
+    par_set.trail_0_5psi=low_level_exp_0_1(par_set.trail_0_5psi,5);
+    par_set.trail_5_25psi=low_level_exp_0_1(par_set.trail_5_25psi,6);
+    par_set.trail_10_25psi=low_level_exp_0_1(par_set.trail_10_25psi,7);
+    par_set.trail_15_25psi=low_level_exp_0_1(par_set.trail_15_25psi,8);
+    par_set.trail_20_25psi=low_level_exp_0_1(par_set.trail_20_25psi,9);
     save('raw_data.mat','par_set');
     fprintf( 'Saved \n' )
 else
@@ -36,4 +44,8 @@ end
 %% Calculate dot_pm, segment inflation and deflation process
 par_set=func_low_level_full_set(par_set);
 %% Plot Overlay Results
-func_plot_low_level_overlayResult(par_set);
+% func_plot_low_level_overlayResult(par_set);
+%% Estimate controller parameters d_pm=a * pm + b * pd
+par_set=func_EstiamtePressCntrlParams_noplot(par_set);
+%% Compare averaged model with different trails
+par_set=func_CompareAveragedModel(par_set);
