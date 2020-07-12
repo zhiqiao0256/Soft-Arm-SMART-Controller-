c41bd5771576ad0a6d7bf5e2bb46d79b3ee4eb1a
zhiqiao0256 <zhiqiao0256@gmail.com>
2020-07-12 16:36:46 -0700

    Update rigid_4_joints_toolbox_test.m

--- rigid_4_joints_toolbox_test.m
+++ rigid_4_joints_toolbox_test.m
@@ -1,6 +1,6 @@
-close all
-clear all
-clc
+% close all
+% clear all
+% clc
 %% Create Rigid Body Obj.
 robot = robotics.RigidBodyTree('DataFormat','column','MaxNumBodies',3);
 %J1
@@ -28,14 +28,13 @@ addBody(robot, body, 'link2');
 body = robotics.RigidBody('link4');
 joint = robotics.Joint('joint4', 'revolute');
 setFixedTransform(joint,trvec2tform([0 0 0]));
-setFixedTransform(joint,[0 -pi/2 0 0],'dh');
+setFixedTransform(joint,[0 0 0 0],'dh');
 body.Joint = joint;
 addBody(robot, body, 'link3');
 
 %% RP-PR 4-DOFs
 % phi_rad=deg2rad(90);
-theta_rad=deg2rad(90);
+theta_rad=deg2rad(-90);
 showdetails(robot)
-figure(1)
-show(robot,[-theta_rad/2,0.17,0.17,-theta_rad/2]');
-
+figure
+show(robot,[theta_rad/2,0.17,0.17,theta_rad/2]');
