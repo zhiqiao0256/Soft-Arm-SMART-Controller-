1179d51473a3db1dca4ee04f6dc0e23b96a75805
zhiqiao0256 <zhiqiao0256@gmail.com>
2020-07-28 09:50:06 -0700

    Create myode.m

---
+++ myode.m
@@ -0,0 +1,4 @@
+function dydt = myode(t,y,ft,f,gt,g)
+f = interp1(ft,f,t); % Interpolate the data set (ft,f) at time t
+g = interp1(gt,g,t); % Interpolate the data set (gt,g) at time t
+dydt = -f.*y + g; % Evaluate ODE at time t
