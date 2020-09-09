2e2fa2736af1b73c2a95b1e00ea7aacb8b155087
zhiqiao0256 <zhiqiao0256@gmail.com>
2020-09-09 11:14:18 -0700

    Create func3ptFilter.m

---
+++ func3ptFilter.m
@@ -0,0 +1,14 @@
+function trainSet= func3ptFilter(trainSet)
+windowSize = 3; 
+filter_b = (1/windowSize)*ones(1,windowSize);
+filter_a = 1;
+trainSet.velocity_phi_rad=zeros(length(trainSet.phi_rad),1);
+trainSet.velocity_phi_rad(2:end)=filter(filter_b,filter_a,(trainSet.phi_rad(2:end)-trainSet.phi_rad(1:end-1))/(1/20));
+trainSet.acc_phi_rad=zeros(length(trainSet.phi_rad),1);
+trainSet.acc_phi_rad(2:end)=filter(filter_b,filter_a,(trainSet.velocity_phi_rad(2:end)-trainSet.velocity_phi_rad(1:end-1))/(1/20));
+
+trainSet.velocity_theta_rad=zeros(length(trainSet.theta_rad),1);
+trainSet.velocity_theta_rad(2:end)=smooth((trainSet.theta_rad(2:end)-trainSet.theta_rad(1:end-1))/(1/20));
+trainSet.acc_theta_rad=zeros(length(trainSet.theta_rad),1);
+trainSet.acc_theta_rad(2:end)=smooth((trainSet.velocity_theta_rad(2:end)-trainSet.velocity_theta_rad(1:end-1))/(1/20));
+end
