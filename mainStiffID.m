9080417dea3fdc0d92e84b961f998b115dc4f8a7
zhiqiao0256 <zhiqiao0256@gmail.com>
2020-09-14 21:22:44 -0700

    Update mainStiffID.m

--- mainStiffID.m
+++ mainStiffID.m
@@ -20,6 +20,11 @@ par_set.trial2=[];
 par_set.trial3=[];
 par_set.trial4=[];
 par_set.trial5=[];
+par_set.trial6=[];
+par_set.trial7=[];
+par_set.trial8=[];
+par_set.trial9=[];
+
 
 
 % Geometric para.
@@ -40,20 +45,16 @@ end
 fprintf('System initialization done \n')
 %% Read txt file or mat file
 if par_set.flag_read_exp==1
-
-%     par_set.trial_4_25psi=func_high_level_exp(par_set.trial_4_25psi,10);
-%     par_set.trial_3_25psi=func_high_level_exp(par_set.trial_3_25psi,9);
-%     par_set.trial_2_25psi=func_high_level_exp(par_set.trial_2_25psi,8);
-%     par_set.trial_1_25psi=func_high_level_exp(par_set.trial_1_25psi,7);
-%     par_set.trial_0_25psi=func_high_level_exp(par_set.trial_0_25psi,6);
-    
-    
     par_set.trial1=func_high_level_exp(par_set.trial1,1);
     par_set.trial2=func_high_level_exp(par_set.trial2,2);
     par_set.trial3=func_high_level_exp(par_set.trial3,3);
     par_set.trial4=func_high_level_exp(par_set.trial4,4);
     par_set.trial5=func_high_level_exp(par_set.trial5,5);
-    
+    par_set.trial6=func_high_level_exp(par_set.trial6,6);
+    par_set.trial7=func_high_level_exp(par_set.trial7,7);
+    par_set.trial8=func_high_level_exp(par_set.trial8,8);
+    par_set.trial9=func_high_level_exp(par_set.trial9,9);
+
     save('raw_id_data.mat','par_set');
     fprintf( 'Saved \n' )
 else
@@ -69,6 +70,158 @@ end
 testData=[];
 testData=par_set.trial1;
 testData=funcGreyBoxSysID(testData,par_set);
-testData=func_greyBox(testData);
-%%
+% testData=func_greyBox(testData);
+par_set.trial1=testData;
 
+testData=[];
+testData=par_set.trial2;
+testData=funcGreyBoxSysID(testData,par_set);
+% testData=func_greyBox(testData);
+par_set.trial2=testData;
+
+testData=[];
+testData=par_set.trial3;
+testData=funcGreyBoxSysID(testData,par_set);
+% testData=func_greyBox(testData);
+par_set.trial3=testData;
+
+testData=[];
+testData=par_set.trial4;
+testData=funcGreyBoxSysID(testData,par_set);
+% testData=func_greyBox(testData);
+par_set.trial4=testData;
+
+testData=[];
+testData=par_set.trial5;
+testData=funcGreyBoxSysID(testData,par_set);
+% testData=func_greyBox(testData);
+par_set.trial5=testData;
+
+testData=[];
+testData=par_set.trial6;
+testData=funcGreyBoxSysID(testData,par_set);
+% testData=func_greyBox(testData);
+par_set.trial6=testData;
+
+testData=[];
+testData=par_set.trial7;
+testData=funcGreyBoxSysID(testData,par_set);
+% testData=func_greyBox(testData);
+par_set.trial7=testData;
+
+testData=[];
+testData=par_set.trial8;
+testData=funcGreyBoxSysID(testData,par_set);
+% testData=func_greyBox(testData);
+par_set.trial8=testData;
+
+testData=[];
+testData=par_set.trial9;
+testData=funcGreyBoxSysID(testData,par_set);
+% testData=func_greyBox(testData);
+par_set.trial9=testData;
+%% crossValidation MPa
+testData=par_set.trial1;
+testData2=par_set.trial2;
+funcCompareTwoGreyBoxModelMPa(testData,testData2)
+
+testData2=par_set.trial3;
+funcCompareTwoGreyBoxModelMPa(testData,testData2)
+
+testData2=par_set.trial4;
+funcCompareTwoGreyBoxModelMPa(testData,testData2)
+
+testData2=par_set.trial5;
+funcCompareTwoGreyBoxModelMPa(testData,testData2)
+
+%% parameter distribution Mpa
+figure
+subplot(3,1,1)
+plot([par_set.trial1.pi_grey(1);par_set.trial2.pi_grey(1);par_set.trial3.pi_grey(1);par_set.trial4.pi_grey(1);par_set.trial5.pi_grey(1);...
+                par_set.trial6.pi_grey(1);par_set.trial7.pi_grey(1);par_set.trial8.pi_grey(1);par_set.trial9.pi_grey(1);],'o');
+ylabel('\alpha')
+subplot(3,1,2)
+plot([par_set.trial1.pi_grey(2);par_set.trial2.pi_grey(2);par_set.trial3.pi_grey(2);par_set.trial4.pi_grey(2);par_set.trial5.pi_grey(2);...
+                par_set.trial6.pi_grey(2);par_set.trial7.pi_grey(2);par_set.trial8.pi_grey(2);par_set.trial9.pi_grey(2);],'o');
+ylabel('k')
+subplot(3,1,3)
+plot([par_set.trial1.pi_grey(3);par_set.trial2.pi_grey(3);par_set.trial3.pi_grey(3);par_set.trial4.pi_grey(3);par_set.trial5.pi_grey(3);...
+                par_set.trial6.pi_grey(3);par_set.trial7.pi_grey(3);par_set.trial8.pi_grey(3);par_set.trial9.pi_grey(3);],'o');
+ylabel('b')
+
+%% Average Model MPa
+meanAlpha=mean([par_set.trial1.pi_grey(1);par_set.trial2.pi_grey(1);par_set.trial3.pi_grey(1);par_set.trial4.pi_grey(1);par_set.trial5.pi_grey(1);...
+                par_set.trial6.pi_grey(1);par_set.trial7.pi_grey(1);par_set.trial8.pi_grey(1);par_set.trial9.pi_grey(1);]);
+meanK=    mean([par_set.trial1.pi_grey(2);par_set.trial2.pi_grey(2);par_set.trial3.pi_grey(2);par_set.trial4.pi_grey(2);par_set.trial5.pi_grey(2);...
+                par_set.trial6.pi_grey(2);par_set.trial7.pi_grey(2);par_set.trial8.pi_grey(2);par_set.trial9.pi_grey(2);]);
+meanB=    mean([par_set.trial1.pi_grey(3);par_set.trial2.pi_grey(3);par_set.trial3.pi_grey(3);par_set.trial4.pi_grey(3);par_set.trial5.pi_grey(3);...
+                par_set.trial6.pi_grey(3);par_set.trial7.pi_grey(3);par_set.trial8.pi_grey(3);par_set.trial9.pi_grey(3);]);
+avgModel.pi_grey=[meanAlpha,meanK,meanB];
+testData2=par_set.trial1;
+funcCompareAverageModel(avgModel,testData2);
+testData2=par_set.trial2;
+funcCompareAverageModel(avgModel,testData2);
+testData2=par_set.trial3;
+funcCompareAverageModel(avgModel,testData2);
+testData2=par_set.trial4;
+funcCompareAverageModel(avgModel,testData2);
+testData2=par_set.trial5;
+funcCompareAverageModel(avgModel,testData2);
+testData2=par_set.trial6;
+funcCompareAverageModel(avgModel,testData2);
+testData2=par_set.trial7;
+funcCompareAverageModel(avgModel,testData2);
+testData2=par_set.trial8;
+funcCompareAverageModel(avgModel,testData2);
+testData2=par_set.trial9;
+funcCompareAverageModel(avgModel,testData2);
+%% CrossValidation psi
+testData2=par_set.trial2;
+% testData2=func_getPhiThetaBfromXYZ(testData2,par_set); 
+% testData2= func3ptFilter(testData2);
+funcCompareTwoGreyBoxModel(testData,testData2);
+
+testData2=par_set.trial3;
+testData2=func_getPhiThetaBfromXYZ(testData2,par_set); 
+testData2= func3ptFilter(testData2);
+funcCompareTwoGreyBoxModel(testData,testData2);
+
+testData2=par_set.trial4;
+testData2=func_getPhiThetaBfromXYZ(testData2,par_set); 
+testData2= func3ptFilter(testData2);
+funcCompareTwoGreyBoxModel(testData,testData2);
+
+testData2=par_set.trial5;
+testData2=func_getPhiThetaBfromXYZ(testData2,par_set); 
+testData2= func3ptFilter(testData2);
+funcCompareTwoGreyBoxModel(testData,testData2);
+%% parameter distribution
+figure
+subplot(3,1,1)
+plot([par_set.trail1.pi_grey(1);par_set.trail2.pi_grey(1);par_set.trail3.pi_grey(1);par_set.trail4.pi_grey(1);par_set.trail5.pi_grey(1);],'o');
+ylabel('\alpha')
+subplot(3,1,2)
+plot([par_set.trail1.pi_grey(2);par_set.trail2.pi_grey(2);par_set.trail3.pi_grey(2);par_set.trail4.pi_grey(2);par_set.trail5.pi_grey(2);],'o');
+ylabel('k')
+subplot(3,1,3)
+plot([par_set.trail1.pi_grey(3);par_set.trail2.pi_grey(3);par_set.trail3.pi_grey(3);par_set.trail4.pi_grey(3);par_set.trail5.pi_grey(3);],'o');
+ylabel('b')
+%% Average Model
+meanAlpha=mean([par_set.trail1.pi_grey(1);par_set.trail2.pi_grey(1);par_set.trail3.pi_grey(1);par_set.trail4.pi_grey(1);par_set.trail5.pi_grey(1);]);
+meanK=mean([par_set.trail1.pi_grey(2);par_set.trail2.pi_grey(2);par_set.trail3.pi_grey(2);par_set.trail4.pi_grey(2);par_set.trail5.pi_grey(2);]);
+meanB=mean([par_set.trail1.pi_grey(3);par_set.trail2.pi_grey(3);par_set.trail3.pi_grey(3);par_set.trail4.pi_grey(3);par_set.trail5.pi_grey(3);]);
+avgModel.pi_grey=[meanAlpha,meanK,meanB];
+testData2=par_set.trail1;
+funcCompareTwoGreyBoxModel(avgModel,testData2);
+testData2=par_set.trail2;
+funcCompareTwoGreyBoxModel(avgModel,testData2);
+testData2=par_set.trail3;
+funcCompareTwoGreyBoxModel(avgModel,testData2);
+testData2=par_set.trail4;
+funcCompareTwoGreyBoxModel(avgModel,testData2);
+testData2=par_set.trail5;
+funcCompareTwoGreyBoxModel(avgModel,testData2);
+%% Full Model
+nlgr=funcBuildFullModel();
+testData=[];
+testData=par_set.trial1;
