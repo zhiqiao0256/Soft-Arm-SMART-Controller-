5d03fe4e6a950049b75c559b13a00355d84456cf
zhiqiao0256 <zhiqiao0256@gmail.com>
2020-09-24 19:40:20 -0700

    Update mainStiffID.m

--- mainStiffID.m
+++ mainStiffID.m
@@ -13,11 +13,11 @@ par_set.flag_plot_rawData = 0;
 %flag for read txt file or mat file 1: txt 0: mat
 par_set.flag_read_exp = 1;
 %flag for plotting moving constant layer
-par_set.flag_plot_movingCC =0;
+par_set.flag_plot_movingCC = 0;
 %flag for plotting fwd kinematic results
-par_set.plot_fwdKinematic =0;
+par_set.plot_fwdKinematic = 0;
 % Check data readme.txt for detail input reference
-par_set.Ts=1/20;
+par_set.Ts=1/40;
 % Geometric para.
 par_set.trianlge_length=70*1e-03;% fabric triangle edge length
 par_set.L=0.17;%actuator length
@@ -155,60 +155,61 @@ par_set.trial15=testData;
 
 beep;
 %% crossValidation MPa
-testData=par_set.trial1;
-testData2=par_set.trial2;
+testData=par_set.trial2;
+testData2=par_set.trial1;
+
+funcCompareTwoGreyBoxModelMPa(testData,testData2)
+
+testData2=par_set.trial7;
 funcCompareTwoGreyBoxModelMPa(testData,testData2)
 
 testData2=par_set.trial3;
-funcCompareTwoGreyBoxModelMPa(testData,testData2)
-
-testData2=par_set.trial4;
 funcCompareTwoGreyBoxModelMPa(testData,testData2)
 
 testData2=par_set.trial5;
 funcCompareTwoGreyBoxModelMPa(testData,testData2)
 
 %% parameter distribution Mpa
-figure
-subplot(3,1,1)
-plot([par_set.trial1.pi_grey(1);par_set.trial2.pi_grey(1);par_set.trial3.pi_grey(1);par_set.trial4.pi_grey(1);par_set.trial5.pi_grey(1);...
-                par_set.trial6.pi_grey(1);par_set.trial7.pi_grey(1);par_set.trial8.pi_grey(1);par_set.trial9.pi_grey(1);],'o');
-ylabel('\alpha')
-subplot(3,1,2)
-plot([par_set.trial1.pi_grey(2);par_set.trial2.pi_grey(2);par_set.trial3.pi_grey(2);par_set.trial4.pi_grey(2);par_set.trial5.pi_grey(2);...
-                par_set.trial6.pi_grey(2);par_set.trial7.pi_grey(2);par_set.trial8.pi_grey(2);par_set.trial9.pi_grey(2);],'o');
-ylabel('k')
-subplot(3,1,3)
-plot([par_set.trial1.pi_grey(3);par_set.trial2.pi_grey(3);par_set.trial3.pi_grey(3);par_set.trial4.pi_grey(3);par_set.trial5.pi_grey(3);...
-                par_set.trial6.pi_grey(3);par_set.trial7.pi_grey(3);par_set.trial8.pi_grey(3);par_set.trial9.pi_grey(3);],'o');
-ylabel('b')
+% figure
+% subplot(3,1,1)
+% plot([par_set.trial1.pi_grey(1);par_set.trial2.pi_grey(1);par_set.trial3.pi_grey(1);par_set.trial4.pi_grey(1);par_set.trial5.pi_grey(1);...
+%                 par_set.trial6.pi_grey(1);par_set.trial7.pi_grey(1);par_set.trial8.pi_grey(1);par_set.trial9.pi_grey(1);],'o');
+% ylabel('\alpha')
+% subplot(3,1,2)
+% plot([par_set.trial1.pi_grey(2);par_set.trial2.pi_grey(2);par_set.trial3.pi_grey(2);par_set.trial4.pi_grey(2);par_set.trial5.pi_grey(2);...
+%                 par_set.trial6.pi_grey(2);par_set.trial7.pi_grey(2);par_set.trial8.pi_grey(2);par_set.trial9.pi_grey(2);],'o');
+% ylabel('k')
+% subplot(3,1,3)
+% plot([par_set.trial1.pi_grey(3);par_set.trial2.pi_grey(3);par_set.trial3.pi_grey(3);par_set.trial4.pi_grey(3);par_set.trial5.pi_grey(3);...
+%                 par_set.trial6.pi_grey(3);par_set.trial7.pi_grey(3);par_set.trial8.pi_grey(3);par_set.trial9.pi_grey(3);],'o');
+% ylabel('b')
 
-%% Average Model MPa
-meanAlpha=mean([par_set.trial1.pi_grey(1);par_set.trial2.pi_grey(1);par_set.trial3.pi_grey(1);par_set.trial4.pi_grey(1);par_set.trial5.pi_grey(1);...
-                par_set.trial6.pi_grey(1);par_set.trial7.pi_grey(1);par_set.trial8.pi_grey(1);par_set.trial9.pi_grey(1);]);
-meanK=    mean([par_set.trial1.pi_grey(2);par_set.trial2.pi_grey(2);par_set.trial3.pi_grey(2);par_set.trial4.pi_grey(2);par_set.trial5.pi_grey(2);...
-                par_set.trial6.pi_grey(2);par_set.trial7.pi_grey(2);par_set.trial8.pi_grey(2);par_set.trial9.pi_grey(2);]);
-meanB=    mean([par_set.trial1.pi_grey(3);par_set.trial2.pi_grey(3);par_set.trial3.pi_grey(3);par_set.trial4.pi_grey(3);par_set.trial5.pi_grey(3);...
-                par_set.trial6.pi_grey(3);par_set.trial7.pi_grey(3);par_set.trial8.pi_grey(3);par_set.trial9.pi_grey(3);]);
-avgModel.pi_grey=[meanAlpha,meanK,meanB];
-testData2=par_set.trial1;
-funcCompareAverageModel(avgModel,testData2);
-testData2=par_set.trial2;
-funcCompareAverageModel(avgModel,testData2);
-testData2=par_set.trial3;
-funcCompareAverageModel(avgModel,testData2);
-testData2=par_set.trial4;
-funcCompareAverageModel(avgModel,testData2);
-testData2=par_set.trial5;
-funcCompareAverageModel(avgModel,testData2);
-testData2=par_set.trial6;
-funcCompareAverageModel(avgModel,testData2);
-testData2=par_set.trial7;
-funcCompareAverageModel(avgModel,testData2);
-testData2=par_set.trial8;
-funcCompareAverageModel(avgModel,testData2);
-testData2=par_set.trial9;
-funcCompareAverageModel(avgModel,testData2);
+% %% Average Model MPa
+% meanAlpha=mean([par_set.trial1.pi_grey(1);par_set.trial2.pi_grey(1);par_set.trial3.pi_grey(1);par_set.trial4.pi_grey(1);par_set.trial5.pi_grey(1);...
+%                 par_set.trial6.pi_grey(1);par_set.trial7.pi_grey(1);par_set.trial8.pi_grey(1);par_set.trial9.pi_grey(1);]);
+% meanK=    mean([par_set.trial1.pi_grey(2);par_set.trial2.pi_grey(2);par_set.trial3.pi_grey(2);par_set.trial4.pi_grey(2);par_set.trial5.pi_grey(2);...
+%                 par_set.trial6.pi_grey(2);par_set.trial7.pi_grey(2);par_set.trial8.pi_grey(2);par_set.trial9.pi_grey(2);]);
+% meanB=    mean([par_set.trial1.pi_grey(3);par_set.trial2.pi_grey(3);par_set.trial3.pi_grey(3);par_set.trial4.pi_grey(3);par_set.trial5.pi_grey(3);...
+%                 par_set.trial6.pi_grey(3);par_set.trial7.pi_grey(3);par_set.trial8.pi_grey(3);par_set.trial9.pi_grey(3);]);
+% avgModel.pi_grey=[meanAlpha,meanK,meanB];
+% testData2=par_set.trial1;
+% funcCompareAverageModel(avgModel,testData2);
+% testData2=par_set.trial2;
+% funcCompareAverageModel(avgModel,testData2);
+% testData2=par_set.trial3;
+% funcCompareAverageModel(avgModel,testData2);
+% testData2=par_set.trial4;
+% funcCompareAverageModel(avgModel,testData2);
+% testData2=par_set.trial5;
+% funcCompareAverageModel(avgModel,testData2);
+% testData2=par_set.trial6;
+% funcCompareAverageModel(avgModel,testData2);
+% testData2=par_set.trial7;
+% funcCompareAverageModel(avgModel,testData2);
+% testData2=par_set.trial8;
+% funcCompareAverageModel(avgModel,testData2);
+% testData2=par_set.trial9;
+% funcCompareAverageModel(avgModel,testData2);
 
 %% bar plot for SMC
 par_set.allAlpha=([par_set.trial1.pi_grey(1);par_set.trial2.pi_grey(1);par_set.trial3.pi_grey(1);...
@@ -228,21 +229,44 @@ par_set.allB=([par_set.trial1.pi_grey(3);par_set.trial2.pi_grey(3);par_set.trial
             par_set.trial7.pi_grey(3);par_set.trial8.pi_grey(3);par_set.trial9.pi_grey(3);...
             par_set.trial10.pi_grey(3);par_set.trial11.pi_grey(3);par_set.trial12.pi_grey(3);...
             par_set.trial13.pi_grey(3);par_set.trial14.pi_grey(3);par_set.trial15.pi_grey(3);]);
-     
-x1=par_set.allK;
+
+par_set.allAlpha2=([par_set.trial1.pi_grey2(1);par_set.trial2.pi_grey2(1);par_set.trial3.pi_grey2(1);...
+                par_set.trial4.pi_grey2(1);par_set.trial5.pi_grey2(1);par_set.trial6.pi_grey2(1);...
+                par_set.trial7.pi_grey2(1);par_set.trial8.pi_grey2(1);par_set.trial9.pi_grey2(1);...
+                par_set.trial10.pi_grey2(1);par_set.trial11.pi_grey2(1);par_set.trial12.pi_grey2(1);...
+                par_set.trial13.pi_grey2(1);par_set.trial14.pi_grey2(1);par_set.trial15.pi_grey2(1);]);
+            
+par_set.allK2=([par_set.trial1.pi_grey2(2);par_set.trial2.pi_grey2(2);par_set.trial3.pi_grey2(2);...
+            par_set.trial4.pi_grey2(2);par_set.trial5.pi_grey2(2);par_set.trial6.pi_grey2(2);...
+            par_set.trial7.pi_grey2(2);par_set.trial8.pi_grey2(2);par_set.trial9.pi_grey2(2);...
+            par_set.trial10.pi_grey2(2);par_set.trial11.pi_grey2(2);par_set.trial12.pi_grey2(2);...
+            par_set.trial13.pi_grey2(2);par_set.trial14.pi_grey2(2);par_set.trial15.pi_grey2(2);]);
+        
+par_set.allB2=([par_set.trial1.pi_grey2(3);par_set.trial2.pi_grey2(3);par_set.trial3.pi_grey2(3);...
+            par_set.trial4.pi_grey2(3);par_set.trial5.pi_grey2(3);par_set.trial6.pi_grey2(3);...
+            par_set.trial7.pi_grey2(3);par_set.trial8.pi_grey2(3);par_set.trial9.pi_grey2(3);...
+            par_set.trial10.pi_grey2(3);par_set.trial11.pi_grey2(3);par_set.trial12.pi_grey2(3);...
+            par_set.trial13.pi_grey2(3);par_set.trial14.pi_grey2(3);par_set.trial15.pi_grey2(3);]);        
+x1=[par_set.allK;par_set.allK2];
 SEM = std(x1)/sqrt(length(x1));               % Standard Error
 ts = tinv([0.025  0.975],length(x1)-1);% T-Score
 CI951 = max(ts*SEM);% Confidence Intervals
+CI951=mean(x1)-std(x1)*1.96/sqrt(length(x1));
+meanK=mean(x1);
 
-x2=par_set.allB;
+x2=[par_set.allB;par_set.allB2];
 SEM = std(x2)/sqrt(length(x2));               % Standard Error
 ts = tinv([0.025  0.975],length(x2)-1);% T-Score
 CI952 =max(ts*SEM);% Confidence Intervals
+CI952=mean(x2)-std(x2)*1.96/sqrt(length(x2));
+meanB=mean(x2);
 
-x3=par_set.allAlpha;
+x3=[par_set.allAlpha;par_set.allAlpha2];
 SEM = std(x3)/sqrt(length(x3));               % Standard Error
 ts = tinv([0.025  0.975],length(x3)-1);% T-Score
 CI953 =max(ts*SEM);% Confidence Intervals
+CI953=mean(x3)-std(x3)*1.96/sqrt(length(x3));
+meanAlpha=mean(x3);
 %% CI95%
 fp=figure('Position',[100,100,600,300]);
 f3=plot(1,x1,'k*');
@@ -273,6 +297,7 @@ xticks([1,2,3])
 xticklabels({'k','b','\alpha'})
 fp.CurrentAxes.FontWeight='Bold';
 fp.CurrentAxes.FontSize=16;
+% ylim([0,2])
 % for i =1:length(x1)
 %     if x1(i)<mean(x1)-CI951 || x1(i)>mean(x1)+CI951
 %         plot(1,x1(i),'ko')
@@ -291,11 +316,11 @@ fp.CurrentAxes.FontSize=16;
 %         hold on
 %     end
 % end
-%%
+
 figure
 subplot(3,1,1)
-histogram(par_set.allK)
+histogram(x1)
 subplot(3,1,2)
-histogram(par_set.allB)
+histogram(x2)
 subplot(3,1,3)
-histogram(par_set.allAlpha)
+histogram(x3)
