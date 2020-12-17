5187f78a1a28b70ef702b66aa7c7f03f40fbb738
zhiqiao0256 <zhiqiao0256@gmail.com>
2020-12-17 15:49:07 -0700

    SMCwithMatchedDistrubance

--- funcSimDOBmatchDist.m
+++ funcSimDOBmatchDist.m
@@ -10,12 +10,13 @@ timeArray=[0:Ts:180]';%sec
 x1=zeros(length(timeArray),1); %state variable theta
 x2=zeros(length(timeArray),1); %state variable dtheta
 x2_filt=x2;% moving average filter
+x2_filter=zeros(3,1);
 
 dx1=zeros(length(timeArray),1); % dot theta
 dx2=zeros(length(timeArray),1); % double dot theta
 e=zeros(length(timeArray),1); % e = x1-xd
 de=zeros(length(timeArray),1);% dot e 
-s=zeros(length(timeArray),1); % s = dot_e + lambda*e
+% smc_s=zeros(length(timeArray),1); % s = dot_e + lambda*e
 
 
 
@@ -24,17 +25,23 @@ g=9.8;       % gravity
 L=par_set.L;      % segment length
 
 
-% Input signal
+%% Input signal
+%%% Sine
 Amp=deg2rad(10);
 Boff=deg2rad(-30);
-freq=0.1;%Hz
+freq=0.01;%Hz
 xd=-Amp*sin(2*pi*freq*timeArray)+Boff;
 dxd=-Amp*(2*pi*freq)*cos(2*pi*freq*timeArray);
 ddxd=Amp*(2*pi*freq)^2*sin(2*pi*freq*timeArray);
 pd1_MPa=zeros(length(timeArray),1);
+%%% Step
+% xd=0.0*sin(2*pi*freq*timeArray)+Boff;
+% dxd=0.0*(2*pi*freq)*cos(2*pi*freq*timeArray);
+% ddxd=0.0*(2*pi*freq)^2*sin(2*pi*freq*timeArray);
+% pd1_MPa=zeros(length(timeArray),1);
+
 %%%% Initial values
 x1(1)=testData.theta_rad(end-1);
-x1(1)=xd(1)+0.1;
 r0=mean(testData.beta);
 phi=mean(testData.phi_rad);
 phi=mean(testData.phi_rad);
@@ -51,7 +58,7 @@ k=par_set.meanK;
 b=par_set.meanB;
 
 %%% smc tunning parameters
-smc_lambda=5;
+smc_lambda=10;
 smc_epsilon=1;
 %%%% system uncertainty
 Km=par_set.maxK-k;
@@ -75,21 +82,21 @@ Alphamax =Alpham;
 Alphamin= -Alpham;
 deltaAlpha = (Alphamax-Alphamin).*rand(1,1) + Alphamin;
 %%% Max uncertainty
-% deltaD=Dmax;
-% deltaK=Kmax*0.01;
-% deltaAlpha=Alphamax;
+deltaD=Dmax*0.0;
+deltaK=Kmax*0.1;
+deltaAlpha=Alphamax*00;
 
-deltaD=0;
-deltaK=0;
-deltaAlpha=0;
-% deltaD=Dmax;
-deltaK=Kmax;
-% deltaAlpha=Alphamax;
+% deltaD=0;
+% deltaK=0;
+% deltaAlpha=0;
+
 %%%% disturbance
 tau_d=zeros(length(timeArray),1);
-tau_d(10/Ts:end,1)=1.0;
-disturb=zeros(length(timeArray),1);
-disturb_est=zeros(length(timeArray),1);
+tau_d(10/Ts:end,1)=-.1;
+lumped_disturb=zeros(length(timeArray),1);
+lumped_disturb_est=zeros(length(timeArray),1);
+lumped_disturb_torque=zeros(length(timeArray),1);
+lumped_disturb_est_torque=zeros(length(timeArray),1);
 %%%%%% Matched Disturbance estimation
 %%%% d_est= z_est + p(s)
 %%%% dot_z_est = -dp/dt*()
@@ -119,37 +126,36 @@ for i=1:length(timeArray)-1
     G_simp=-(g*m0*(L*sin(theta) + r0*theta^2*cos(theta) - L*theta*cos(theta)))/(2*theta^2);
     f1=-M\(k*x1(i,1) +(b+C_simp)*x2_filt(i,1)+ G_simp);
     b_x=alpha/M;
-    %%% NDOB calculation
-    ndob_d_est(i,1)=ndob_p(i,1)+ndob_lx*[x1(i,1);x2_filt(i,1)];
-    disturb_est(i,1)=ndob_d_est(i,1);
-    
-    smc_s=de(i,1) + smc_lambda*e(i,1)+ndob_d_est(i,1);
-%     max_disturbance_est_err=Km*(x1(i,1)/M);
-    smc_eta=(smc_lambda+ndob_lx*ndob_g2)*max_disturbance_est_err+0.01;
+    %%% Update SMC
+    smc_s=de(i,1)+smc_lambda*e(i,1);
+    smc_eta=0.5*abs(smc_s)+0.1;
     if smc_s > smc_epsilon
         smc_s_sat=smc_s/abs(smc_s);
     else
         smc_s_sat=smc_s/smc_epsilon;
     end
-    u(i,1)=(-1/b_x*(f1- ddxd(i,1)+ smc_lambda*x2_filt(i,1)-smc_lambda*dxd(i,1)+...
-                smc_lambda*ndob_d_est(i,1)+smc_eta*smc_s_sat));
-    disturb(i,1)=(deltaK*x1(i,1)+deltaD*x2_filt(i,1)+deltaAlpha*alpha*u(i,1)+tau_d(i,1));
-    pd1_MPa(i,1)=(u(i,1)-(0.5*sin(phi)+0.5*sqrt(3)*cos(phi))*pm2_MPa+sin(phi)*pm3_MPa)/(0.5*sin(phi)-0.5*sqrt(3)*cos(phi));
-    ndob_f=[x2_filt(i,1);f1];
-    ndob_g1=[0;b_x];
-    %%% Update px lx
+    u_eq(i,1)=-1/b_x*(f1+smc_lambda*de(i,1)-ddxd(i,1));
+    u_n(i,1)=-1/b_x*ndob_d_est(i,1);
+    u_s(i,1)=-1/b_x*smc_eta*smc_s_sat;
+    u(i,1)=u_eq(i,1)+u_n(i,1)+u_s(i,1);
+    lumped_disturb(i,1)=M\(deltaK*x1(i,1)+deltaD*x2_filt(i,1)+alpha*deltaAlpha*u(i,1)+tau_d(i,1));
+    lumped_disturb_torque(i,1)=(deltaK*x1(i,1)+deltaD*x2_filt(i,1)+alpha*deltaAlpha*u(i,1)+tau_d(i,1));
     %%% Update estimated torque
-    dx1(i,1)=x2(i,1)+disturb(i,1);
-    dx2(i,1)=f1+b_x*u(i,1);
-    ndob_dp(i,1)=-ndob_lx*ndob_g2*ndob_p(i,1)-ndob_lx*(ndob_g2*ndob_lx*[x1(i,1);x2_filt(i,1)]+ndob_f+ndob_g1*u(i,1));
+    dx1(i,1)=x2(i,1);
+    dx2(i,1)=f1+b_x*u(i,1)+lumped_disturb(i,1);
     x1(i+1,1)=x1(i,1)+dx1(i,1)*Ts;
     x2(i+1,1)=x2(i,1)+dx2(i,1)*Ts;
-    ndob_p(i+1,1)=ndob_p(i,1)+ndob_dp(i,1)*Ts;
-
+    %%%% Update Observer
+    ndob_p_of_s=35*smc_s;
+    ndob_dpds=35;
+    ndob_dz_est(i,1)=-ndob_dpds*(b_x*(u_n(i,1)+u_s(i,1))+ndob_d_est(i,1)); 
+    ndob_z_est(i+1,1)=ndob_dz_est(i,1)*Ts+ndob_z_est(i,1);
+    ndob_d_est=ndob_z_est+ndob_p_of_s;
+    ndob_d_est_torque(i,1)=ndob_z_est(i,1)*M;
 end
 % disturb=(Kmax*x1+Dmax*x2_filt+Alphamax*alpha*u+tau_d);
 %% Result compare
-fp=figure('Name','smcAndilc','Position',[100,100,600,400]);
+fp=figure('Name','smcMathcedDist','Position',[500,100,600,600]);
 subplot(4,1,1)
 plot(timeArray(1:end),xd(1:end),'r','LineWidth',2)
 hold on
@@ -170,17 +176,17 @@ title(['x2'])
 ylabel('Anguler Vel.(rad/s)')
 hold on
 subplot(4,1,3)
-plot(timeArray(1:end),pd1_MPa(1:end)*145.038,'r')
+plot(timeArray(1:end-1),u(1:end),'r')
 hold on
 title(['Control Signal u'])
-ylabel('Pd(Psi)')
+ylabel('torque(N\cdot m)')
 xlabel('time')
 subplot(4,1,4)
-plot(timeArray(1:end),disturb_est(1:end),'b','LineWidth',2)
+plot(timeArray(1:end-1),ndob_d_est_torque(1:end),'b','LineWidth',2)
 hold on
-plot(timeArray(1:end),disturb(1:end),'r')
+plot(timeArray(1:end-1),lumped_disturb_torque(1:end-1),'r')
 legend('\Psi_{est}','\Psi_{ref}')
 title(['Lumped Dist. Estimation'])
-ylabel('angle(rad)')
+ylabel('torque(N\cdot m)')
 xlabel('time')
 end
