c554384e554cb4bff03329602c548e8b52b42877
zhiqiao0256 <37387088+zhiqiao0256@users.noreply.github.com>
2021-04-26 11:42:48 -0700

    adddata

--- mainPostProcessing_smcwndos.m
+++ mainPostProcessing_smcwndos.m
@@ -36,11 +36,11 @@ end
 fprintf('System initialization done \n')
 %% Read txt file or mat file
 if par_set.flag_read_exp==1
-    par_set=funcHighLevelExpPositionTracking(par_set,1);
-    par_set=funcHighLevelExpPositionTracking(par_set,2);
+%     par_set=funcHighLevelExpPositionTracking(par_set,1);
+%     par_set=funcHighLevelExpPositionTracking(par_set,2);
     par_set=funcHighLevelExpPositionTracking(par_set,3);
-    par_set=funcHighLevelExpPositionTracking(par_set,4);
-    par_set=funcHighLevelExpPositionTracking(par_set,5);
+%     par_set=funcHighLevelExpPositionTracking(par_set,4);
+%     par_set=funcHighLevelExpPositionTracking(par_set,5);
 %     par_set=funcHighLevelExpPositionTracking(par_set,6);
 %     par_set=funcHighLevelExpPositionTracking(par_set,7);
 %     par_set=funcHighLevelExpPositionTracking(par_set,8);
@@ -52,40 +52,41 @@ else
     fprintf( 'Data loaded \n' );
 end
 %%
-testData= par_set.trial1;
-s_pt=1;e_pt=length(testData.xd_exp(:,2));
-testData = funcPostProcess(testData,s_pt,e_pt);
-rmse=testData.rmse;
-Ep=testData.inputEnergy;
-testData= par_set.trial2;
-s_pt=1;e_pt=length(testData.xd_exp(:,2));
-testData = funcPostProcess(testData,s_pt,e_pt);
-rmse=testData.rmse+rmse;
-Ep=testData.inputEnergy+Ep;
-testData= par_set.trial3;
-s_pt=1;e_pt=length(testData.xd_exp(:,2));
-testData = funcPostProcess(testData,s_pt,e_pt);
-rmse=testData.rmse+rmse;
-Ep=testData.inputEnergy+Ep;
-testData= par_set.trial4;
-s_pt=1;e_pt=length(testData.xd_exp(:,2));
-testData = funcPostProcess(testData,s_pt,e_pt);
-rmse=testData.rmse+rmse;
-Ep=testData.inputEnergy+Ep;
-testData= par_set.trial5;
-s_pt=1;e_pt=length(testData.xd_exp(:,2));
-testData = funcPostProcess(testData,s_pt,e_pt);
-rmse=testData.rmse+rmse;
-Ep=testData.inputEnergy+Ep;
-rmse=rmse/5;
-Ep=Ep/5;
+% testData= par_set.trial1;
+% s_pt=1;e_pt=length(testData.xd_exp(:,2));
+% testData = funcPostProcess(testData,s_pt,e_pt);
+% rmse=testData.rmse;
+% Ep=testData.inputEnergy;
+% testData= par_set.trial2;
+% s_pt=1;e_pt=length(testData.xd_exp(:,2));
+% testData = funcPostProcess(testData,s_pt,e_pt);
+% rmse=testData.rmse+rmse;
+% Ep=testData.inputEnergy+Ep;
+% testData= par_set.trial3;
+% s_pt=1;e_pt=length(testData.xd_exp(:,2));
+% testData = funcPostProcess(testData,s_pt,e_pt);
+% rmse=testData.rmse+rmse;
+% Ep=testData.inputEnergy+Ep;
+% testData= par_set.trial4;
+% s_pt=1;e_pt=length(testData.xd_exp(:,2));
+% testData = funcPostProcess(testData,s_pt,e_pt);
+% rmse=testData.rmse+rmse;
+% Ep=testData.inputEnergy+Ep;
+% testData= par_set.trial5;
+% s_pt=1;e_pt=length(testData.xd_exp(:,2));
+% testData = funcPostProcess(testData,s_pt,e_pt);
+% rmse=testData.rmse+rmse;
+% Ep=testData.inputEnergy+Ep;
+% rmse=rmse/5;
+% Ep=Ep/5;
+return
 %% Segment data1 
 % close all
 testData= par_set.trial1;
-fp=figure('Name','ramp','Position',[100,100,600,800]);
-plot(testData.xd_exp(:,2))
+% fp=figure('Name','ramp','Position',[100,100,600,800]);
+% plot(testData.xd_exp(:,2))
 s_pt=1;e_pt=length(testData.xd_exp(:,2));
-s_pt=405;e_pt=2399;
+% s_pt=1;e_pt=2400;
 %%% fig 1
 % fp=figure('Name','fig1','Position',[100,100,800,600]);
 % subplot(2,1,1)
@@ -118,7 +119,7 @@ hold on
 plot(testData.x1_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.xdNew(s_pt:e_pt),'k','LineStyle',':','LineWidth',2)
 ylabel('Angle (rad)')
 xlabel('Time (second)')
-xlim([0,50])
+% xlim([0,50])
 ylim([-0.7,-0.1])
 legend('\theta_d','\theta','\theta_a','Orientation','horizontal','Location','north')
 fp.CurrentAxes.FontWeight='Bold';
@@ -130,7 +131,7 @@ legend('$\hat{\Delta}$','Interpreter','latex','Orientation','horizontal','Locati
 ylabel('Disturbance (Nm)')
 xlabel('Time (second)')
 ylim([-0.2,2])
-xlim([0,50])
+% xlim([0,50])
 fp.CurrentAxes.FontWeight='Bold';
 fp.CurrentAxes.FontSize=20;
 
@@ -141,65 +142,89 @@ legend('$p_{m_1}$','Interpreter','latex','Orientation','horizontal','Location','
 ylabel('Air Pressrue (MPa)')
 xlabel('Time (second)')
 ylim([0,0.3])
-xlim([0,50])
+% xlim([0,50])
 fp.CurrentAxes.FontWeight='Bold';
 fp.CurrentAxes.FontSize=20;
 testData = funcPostProcess(testData,s_pt,e_pt);
 return
 %% Segment data4 smcndob
 % close all
-testData= par_set.trial4;
+testData= par_set.trial2;
 fp=figure('Name','ramp','Position',[100,100,600,800]);
 plot(testData.xd_exp(:,2))
 s_pt=1;e_pt=length(testData.xd_exp(:,2));
+%%% fig 2
 fp=figure('Name','fig1','Position',[100,100,800,600]);
-subplot(2,1,1)
 plot(testData.xd_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.xd_exp(s_pt:e_pt,2),'r','LineStyle','-','LineWidth',2)
 hold on
 plot(testData.x1_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.x1_exp(s_pt:e_pt,2),'b','LineStyle','-.','LineWidth',2)
 hold on
 plot(testData.x1_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.xdNew(s_pt:e_pt),'k','LineStyle',':','LineWidth',2)
 ylabel('Angle (rad)')
-xlim([0,50])
-ylim([-0.9,-0.1])
+xlabel('Time (second)')
+% xlim([0,50])
+ylim([-0.7,-0.1])
 legend('\theta_d','\theta','\theta_a','Orientation','horizontal','Location','north')
 fp.CurrentAxes.FontWeight='Bold';
-fp.CurrentAxes.FontSize=10;
-subplot(2,1,2)
+fp.CurrentAxes.FontSize=20;
+%%% fig 3
+fp=figure('Name','fig1','Position',[100,100,800,600]);
 plot(testData.x1_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.dist_est_inner_tau(s_pt:e_pt),'r','LineWidth',2)
-legend('\Delta','Orientation','horizontal','Location','northeast')
+legend('$\hat{\Delta}$','Interpreter','latex','Orientation','horizontal','Location','northeast')
 ylabel('Disturbance (Nm)')
 xlabel('Time (second)')
 ylim([-0.2,2])
-xlim([0,50])
+% xlim([0,50])
 fp.CurrentAxes.FontWeight='Bold';
-fp.CurrentAxes.FontSize=10;
+fp.CurrentAxes.FontSize=20;
+
+%%% fig 4
+fp=figure('Name','fig1','Position',[100,100,800,600]);
+plot(testData.x1_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.pm_MPa(s_pt:e_pt,2),'k','LineWidth',2)
+legend('$p_{m_1}$','Interpreter','latex','Orientation','horizontal','Location','northeast')
+ylabel('Air Pressrue (MPa)')
+xlabel('Time (second)')
+ylim([0,0.3])
+% xlim([0,50])
+fp.CurrentAxes.FontWeight='Bold';
+fp.CurrentAxes.FontSize=20;
 testData = funcPostProcess(testData,s_pt,e_pt);
 %% segment data 5 smcndob with switch
-testData= par_set.trial5;
-fp=figure('Name','ramp','Position',[100,100,600,800]);
-plot(testData.xd_exp(:,2))
+testData= par_set.trial3;
 s_pt=1;e_pt=length(testData.xd_exp(:,2));
+%%% fig 2
 fp=figure('Name','fig1','Position',[100,100,800,600]);
-subplot(2,1,1)
 plot(testData.xd_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.xd_exp(s_pt:e_pt,2),'r','LineStyle','-','LineWidth',2)
 hold on
 plot(testData.x1_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.x1_exp(s_pt:e_pt,2),'b','LineStyle','-.','LineWidth',2)
 hold on
 plot(testData.x1_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.xdNew(s_pt:e_pt),'k','LineStyle',':','LineWidth',2)
 ylabel('Angle (rad)')
-xlim([0,50])
-ylim([-0.9,-0.1])
+xlabel('Time (second)')
+% xlim([0,50])
+ylim([-0.7,-0.1])
 legend('\theta_d','\theta','\theta_a','Orientation','horizontal','Location','north')
 fp.CurrentAxes.FontWeight='Bold';
-fp.CurrentAxes.FontSize=10;
-subplot(2,1,2)
+fp.CurrentAxes.FontSize=20;
+%%% fig 3
+fp=figure('Name','fig1','Position',[100,100,800,600]);
 plot(testData.x1_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.dist_est_inner_tau(s_pt:e_pt),'r','LineWidth',2)
-legend('\Delta','Orientation','horizontal','Location','northeast')
+legend('$\hat{\Delta}$','Interpreter','latex','Orientation','horizontal','Location','northeast')
 ylabel('Disturbance (Nm)')
 xlabel('Time (second)')
 ylim([-0.2,2])
-xlim([0,50])
+% xlim([0,50])
 fp.CurrentAxes.FontWeight='Bold';
-fp.CurrentAxes.FontSize=10;
+fp.CurrentAxes.FontSize=20;
+
+%%% fig 4
+fp=figure('Name','fig1','Position',[100,100,800,600]);
+plot(testData.x1_exp(s_pt:e_pt,1)-testData.xd_exp(s_pt,1),testData.pm_MPa(s_pt:e_pt,2),'k','LineWidth',2)
+legend('$p_{m_1}$','Interpreter','latex','Orientation','horizontal','Location','northeast')
+ylabel('Air Pressrue (MPa)')
+xlabel('Time (second)')
+ylim([0,0.3])
+% xlim([0,50])
+fp.CurrentAxes.FontWeight='Bold';
+fp.CurrentAxes.FontSize=20;
 testData = funcPostProcess(testData,s_pt,e_pt);
