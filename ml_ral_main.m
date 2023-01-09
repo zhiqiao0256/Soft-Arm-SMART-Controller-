54faafa77619f6b3cbc2574877eefec1fb9e4e59
zhiqiao0256 <37387088+zhiqiao0256@users.noreply.github.com>
2023-01-09 11:14:39 -0700

    remove not needed code

--- ml_ral_main.m
+++ ml_ral_main.m
@@ -9,7 +9,7 @@ par_set=[];
 %flag for EOM deriviation
 par_set.EOM=0;
 %flag for plot
-par_set.flag_plot_rawData =0;
+par_set.flag_plot_rawData =1;
 %flag for read txt file or mat file 1: txt 0: mat
 par_set.flag_read_exp = 1;
 
@@ -36,4 +36,18 @@ else
     fprintf( 'Loading... \n' );
     load('raw_id_data.mat');
     fprintf( 'Data loaded \n' );
+end
+%% PLot raw data
+testData = par_set.trial1;
+if par_set.flag_plot_rawData == true
+    close all;
+    figure(1)
+    title('xyz pos in cam frame')
+    plot3(testData.rigid_1_pose(:,1),testData.rigid_1_pose(:,2),testData.rigid_1_pose(:,3),'r')
+    hold on
+    plot3(testData.rigid_2_pose(:,1),testData.rigid_2_pose(:,2),testData.rigid_2_pose(:,3),'b')
+    hold on
+    plot3(testData.rigid_3_pose(:,1),testData.rigid_3_pose(:,2),testData.rigid_3_pose(:,3),'k')
+    hold on
+    legend('R1','R2','R3')
 end
